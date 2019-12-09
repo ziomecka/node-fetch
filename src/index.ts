e9392787ee73770c321f93dca74841b3a71ab619
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-12-09 21:52:10 +0100

    Add ./src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { buildNodeFetchPath } from './build-node-fetch-path';
+export { sendInitialData } from './send-initial-data';
+export { getAuthenticationToken } from './get-authentication-token';
+export { getCluster } from './get-cluster';
+export { nodeFetch, NodeFetchOptions } from './node-fetch';
